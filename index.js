465e3c16f2dcd30e87f5b8274385d684d6ea31d6
LIYI <liyibass@gmail.com>
2021-09-02 18:06:50 +0800

    feat: set querystring.stringify to JSON.stringify

--- index.js
+++ index.js
@@ -48,16 +48,15 @@ class NewebPay {
 
     getDecryptedTradeInfo(tradeInfoAES) {
         const decryptedQueryString = this._decryptAES(tradeInfoAES)
-        const decryptedTradeInfo = querystring.parse(decryptedQueryString)
+        const decryptedTradeInfo = JSON.parse(decryptedQueryString)
         return decryptedTradeInfo
     }
 
     _encryptAES(tradeInfo) {
-        const tradeInfoQueryString = querystring.stringify(tradeInfo)
-
+        const tradeInfoString = JSON.stringify(tradeInfo)
         const cipher = crypto.createCipheriv('aes-256-cbc', this.key, this.iv)
         const encrypted = cipher.update(
-            this._addPadding(tradeInfoQueryString),
+            this._addPadding(tradeInfoString),
             'binary',
             'hex'
         )
